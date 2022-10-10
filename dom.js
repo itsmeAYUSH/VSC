9bbefb2edfc971c0b8beed24cd35c5693bec7ea1
itsmeAYUSH <ayushksagar14@gmail.com>
2022-10-10 09:52:19 +0530

    adding index.html and dom.js

---
+++ dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// headerTitle.style.borderBottom = 'solid 4px #000';
+header.style.borderBottom = 'solid 4px #000';
+var item = document.getElementsByClassName('title');
+item[0].style.fontWeight = 'bold';
+item[0].style.color = 'green';
