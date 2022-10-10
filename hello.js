42bc883aa58f066559758c75033a26e0654924a0
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-10-10 10:36:04 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log('sup');
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// headerTitle.style.borderBottom = 'solid 4px #000';
+header.style.borderBottom = 'solid 4px #000';
+var item = document.getElementsByClassName('title');
+item[0].style.fontWeight = 'bold';
+item[0].style.color = 'green';
+var list = document.getElementsByClassName('list-group-item');
+list[2].style.backgroundColor = 'green';
+list[1].style.fontWeight = 'bold';
+
+for(var i=0 ; i<list.length ; i++){
+    list[i].style.backgroundColor = 'green';
+    list[i].style.fontWeight = 'bold'
+}
