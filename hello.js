aa20fcbf171098baa33b99e9a02b819e65ba32c8
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-10-11 12:11:18 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,25 +1,31 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
+var disitem = document.getElementById('discription')
 
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click',removeItem);
 filter.addEventListener('keyup',filterItems);
 
-
 function addItem(e){
 
     e.preventDefault();
 
-    var newItem = document.getElementById('item').value;
+    var newItem = document.getElementById('item').value; 
+    var newDis = document.getElementById('discription').value;
     var li = document.createElement('li');
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(" "+newDis));
     var deleteBtn = document.createElement('button');
+    var editBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
     deleteBtn.appendChild(document.createTextNode('X'));
     li.appendChild(deleteBtn);
     itemList.appendChild(li);
+    editBtn.className = "btn btn-warning btn-sm float-right";
+    editBtn.appendChild(document.createTextNode("edit"));
+    li.appendChild(editBtn);
      
 }
 function removeItem(e){
@@ -32,14 +38,20 @@ function removeItem(e){
 }
 
 function filterItems(e){
+    
     var text = e.target.value.toLowerCase();
     var items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item){
         var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text)!=-1){
+        var disItem = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1 || disItem.toLowerCase().indexOf(text)!=-1){
             item.style.display = 'block';
         }else{
-            item.style.display = 'none';
+            item.style.display = 'none';    
         }
     })
+    
 }
+
+
+
